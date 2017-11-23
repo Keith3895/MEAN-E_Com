e55bcc7eef707cf7e05ef2acbd2a772b1b284de0
Keith Franklin <keith30895@gmail.com>
2017-11-23 08:19:41 +0530

    fixed cart

--- angular-files/src/app/components/cart/cart.component.ts
+++ angular-files/src/app/components/cart/cart.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { MatTableDataSource } from '@angular/material';
 import {Cart} from '../../models/cart';
 import {CartService} from '../../services/cart.service';
 import { AuthService } from '../../services/auth.service';
-import { ChangeDetectorRef } from '@angular/core';
 import { of } from 'rxjs/observable/of'
 import {Router}  from '@angular/router';
 
@@ -17,11 +16,10 @@ import {Router}  from '@angular/router';
 export class CartComponent implements OnInit {
   displayedColumns = ['index', 'product', 'quantity', 'Price'];
   
-  dataSource = new MatTableDataSource(this.cartService.cartContent);
+  dataSource:any;  
   constructor(
     private cartService:CartService,
     private authService:AuthService,
-    private cd: ChangeDetectorRef,
     private router:Router) { }
 
   ngOnInit() {
@@ -29,10 +27,13 @@ export class CartComponent implements OnInit {
     this.cartService.retriveCart().subscribe((data)=>{
       if(data.success){
         this.cartService.cartContent=data.msg;
+        this.dataSource=new MatTableDataSource(this.cartService.cartContent);
         console.log(data.msg);
-        this.router.navigate(['cart']);   
       }
     });
+  }
+  ngAfterViewInit(){
+
   }
   emptyCart(){
     this.cartService.emptyCart().subscribe(data=>{
