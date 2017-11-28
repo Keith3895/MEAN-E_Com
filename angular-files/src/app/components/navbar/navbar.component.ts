200d2d3d42858663e2af6410c6a21fe9bc7081ed
Keith3895 <keith30895@gmail.com>
2017-11-28 17:08:33 +0530

    changes to dashboard and chnages to ard-form placements

--- angular-files/src/app/components/navbar/navbar.component.ts
+++ angular-files/src/app/components/navbar/navbar.component.ts
@@ -3,6 +3,7 @@ import {AuthService} from '../../services/auth.service';
 import {Router}  from '@angular/router';
 // import { RouterLinkActive } from '@angular/router';
 import {ToastService} from '../../services/toast.service';
+import { ObservableMedia, MediaChange } from '@angular/flex-layout';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -10,13 +11,26 @@ import {ToastService} from '../../services/toast.service';
   encapsulation: ViewEncapsulation.None
 })
 export class NavbarComponent implements OnInit {
-
+  hide:Boolean;
+  isMobileView:Boolean;
+  side:any;
   constructor(
     private router: Router,
   	private toastService:ToastService,
-  	private authService:AuthService) { }
+    private authService:AuthService,
+    public media:ObservableMedia) { }
 
   ngOnInit() {
+    this.isMobileView = (this.media.isActive('xs') || this.media.isActive('sm'));
+    this.media.subscribe((change:MediaChange) => {
+      this.isMobileView = (change.mqAlias === 'xs' || change.mqAlias === 'sm');
+      this.hide = this.isMobileView;
+      if(this.isMobileView)
+        this.side = 'over';
+      else
+      this.side = 'side';
+    });
+    this.hide = this.isMobileView;
   }
   isVisibleOnDesktop(){}
   onLogout(){
@@ -25,3 +39,4 @@ export class NavbarComponent implements OnInit {
   	this.router.navigate(['']);
   }
 }
+ 
